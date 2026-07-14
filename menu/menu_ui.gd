ec78f8044c08fcdd8d7fae2917c75f15d9c2e543
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2026-07-14 11:58:35 +0800

    Prevent menu opening during dialogue

--- menu/menu_ui.gd
+++ menu/menu_ui.gd
@@ -59,7 +59,7 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("inventory"):
-		if main_menu.visible or opening_overlay.visible:
+		if main_menu.visible or opening_overlay.visible or Dialogic.current_timeline != null:
 			return
 		if is_open:
 			close()
