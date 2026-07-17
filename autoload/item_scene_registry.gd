52f08d933502de59e93e17a2fe790659684a5dd0
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2026-07-17 14:29:37 +0800

    WIP: Add fish and honey bear-spirit offerings
    Introduces Fish and Honey as new consumable inventory items with assets, pickup scripts/scenes, and registry wiring so they can be spawned and collected. Updates item manager/default player inventory and Dialogic bear spirit timeline variables/branches to support offering either item once per day, with a new satisfied response after trading. Also replaces the old bow item resource and switches the bear spirit node/script base type from StaticBody2D to CharacterBody2D.

--- autoload/item_scene_registry.gd
+++ autoload/item_scene_registry.gd
@@ -2,11 +2,13 @@ extends Node
 
 const PICKUP_SCENES: Dictionary = {
 	"1": preload("res://inventory/scenes/pickup_items/apple.tscn"),
+	"2": preload("res://inventory/scenes/pickup_items/fish.tscn"),
 	"4": preload("res://inventory/scenes/pickup_items/axe.tscn"),
 	"5": preload("res://inventory/scenes/pickup_items/log.tscn"),
 	"6": preload("res://inventory/scenes/pickup_items/sword.tscn"),
 	"7": preload("res://inventory/scenes/pickup_items/stone.tscn"),
-	"8": preload("res://inventory/scenes/pickup_items/hammer.tscn")
+	"8": preload("res://inventory/scenes/pickup_items/hammer.tscn"),
+	"9": preload("res://inventory/scenes/pickup_items/honey.tscn")
 }
 
 func get_scene(item: InvItem) -> PackedScene:
